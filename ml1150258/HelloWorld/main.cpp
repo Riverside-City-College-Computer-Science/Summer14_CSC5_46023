7def4222123d27068a8f3f6812b4626439cd249d
mlehrjava <mark.lehr@rcc.edu>
2014-06-23 12:42:23 -0700

    Signed-off-by: mlehrjava <mark.lehr@rcc.edu>

---
+++ ml1150258/HelloWorld/main.cpp
@@ -0,0 +1,24 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on June 23, 2014, 11:22 AM
+ * Purpose:  First Program
+ */
+
+//System Level Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv){
+    //Output simple text
+    cout << "Hello World" << endl;
+    //Exit stage right!
+    return 0;
+}
