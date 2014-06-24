9d8c4c9d77dbe4225cb8c72e2b4f16b0fc43b3d5
nkalu11 <nkalu11@yahoo.com>
2014-06-24 13:42:34 -0700

    Signed-off-by: nkalu11 <nkalu11@yahoo.com>

---
+++ nk2528025/hjwerwy/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on June 24, 2014, 1:41 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
