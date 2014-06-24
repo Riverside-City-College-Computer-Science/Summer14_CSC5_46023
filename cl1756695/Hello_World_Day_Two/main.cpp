b48f6e1b45aed30b510be00c275fabe9ef818c4a
cphilliplierz <cplierz@gmail.com>
2014-06-24 12:57:58 -0700

    Signed-off-by: cphilliplierz <cplierz@gmail.com>

---
+++ cl1756695/Hello_World_Day_Two/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: C. Phillip Lierz
+ * Purpose: Second Day PreLab
+ * Created on June 24, 2014, 9:58 AM
+ */
+
+//System Level Libraries
+
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Begin Execution
+
+int main(int argc, char** argv) {
+
+    //Output Simple Text
+    
+    cout<<"Hello World."<<endl;
+    
+    cout<<"Hello Death."<<endl;
+    
+    //Finish
+    
+    return 0;
+}
