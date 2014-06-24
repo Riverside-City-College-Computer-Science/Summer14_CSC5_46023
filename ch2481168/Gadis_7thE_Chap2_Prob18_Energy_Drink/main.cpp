105e19e08425160662dde30989a6e076c3666012
chillis777 <connor.hillis@yahoo.com>
2014-06-24 13:42:37 -0700

    Signed-off-by: chillis777 <connor.hillis@yahoo.com>

---
+++ ch2481168/Gadis_7thE_Chap2_Prob18_Energy_Drink/main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   main.cpp
+ * Author: Connor Hillis
+ *
+ * Created on June 24, 2014, 11:27 AM
+ */
+//System Liberary
+#include <cstdlib>
+
+using namespace std;
+
+//User Define Libraries
+
+//Global Constants
+
+ //Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+//Declare and initialize variables
+    unsigned short cusSrvy=12467;//Customers Surveyed
+    float eDrnkp=0.14f;//Percent Energy Drinkers from Customers Surveyed
+    float cDrnkp=0.64f;//Percent of Citrus Drinkers
+    unsigned short enDrnk,citDrnk;//Number of energy
+    
+    //Calculate the number of energy and citrus drinkers
+    enDrnk=cusSrvy*eDrnkp;
+    citDrnk=enDrnk*cDrnkp
+         
+Output the results
+cout<<"Total Number of Customers Surveyed"="<<cus...."
+cout<<"Number of Energy Drinkers"="........"
+cout<<"Numbe of Citrus Drinks"=".........."            
+    return 0;
+}
+
