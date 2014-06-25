e3d8bcbf1a3ef9751c0d08aae043882bbe9a6b63
sjin7 <jsbb_7@hotmail.com>
2014-06-25 13:05:18 -0700

    Signed-off-by: radbradd <braddley.carey@gmail.com> Signed-off-by: sjin7 <jsbb_7@hotmail.com>

---
+++ sj2480130/Hmwk/Savitch_8thEd_Chap1_Prob5/main.cpp
@@ -0,0 +1,37 @@
+/* 
+ * File:   Savitch_8thEd_Chap1_Prob5
+ * Author: Shen Jin
+ * Created on June 25, 2014, 1:09 AM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+//User Defined Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    
+    int num1, num2;
+    int sum = num1 + num2;
+    int prod = num1 * num2;
+    
+    cout << "Please provide two integers.\n";
+    cin >> num1;
+    cin >> num2;
+    cout << "Sum: ";
+    cout << sum;
+    cout << "Product: ";
+    cout << prod;
+    
+    cout << "End of the Program.\n";
+            
+
+    return 0;
+}
+
