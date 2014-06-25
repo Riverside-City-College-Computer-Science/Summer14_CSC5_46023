5a32083fc99d67b38339dfa71ff34c6f5b9989ee
mdaly001 <mdaly001@gmail.com>
2014-06-24 22:15:16 -0700

    Signed-off-by: mdaly001 <mdaly001@gmail.com>

---
+++ md1785525/Hmwk/Gaddis_7thEd_Chap2_Prob14/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: mdaly001
+ * Created on June 24, 2014, 4:49 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+// User Defined Libraries
+
+// Global Constants
+
+// Function Prototypes
+
+// Start Executiong Here:
+int main(int argc, char** argv) {
+    // The following code displays personal information on seperate lines
+    // I will not be using my personal information because this is on GitHub
+
+    cout<<"Name: John Doe"<<endl; // Name
+    cout<<"Address: 12345 NoWhere Street, Littletown, CA, 92345"<<endl; // Address
+    cout<<"Phone: 222-222-2222"<<endl; // Phone Number
+    cout<<"College Major: Computer Science"<<endl; // College Major
+    
+    // Exit
+    return 0;
+}
+
