35a8ed936106579c7b394db79ade2750a04f5828
rlucatero13 <rlucatero@student.rcc.edu>
2014-06-30 13:13:05 -0700

    Signed-off-by: rlucatero13 <RSLuca619@gmail.com> Signed-off-by: rlucatero13 <rlucatero@student.rcc.edu>

---
+++ rl2478631/Truth_Table/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_ARTIFACT_NAME_Debug=rlucatero_truth_table
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/rlucatero_truth_table
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_PACKAGE_NAME_Debug=rlucaterotruthtable.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/rlucaterotruthtable.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_ARTIFACT_NAME_Release=rlucatero_truth_table
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/rlucatero_truth_table
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_PACKAGE_NAME_Release=rlucaterotruthtable.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/rlucaterotruthtable.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
