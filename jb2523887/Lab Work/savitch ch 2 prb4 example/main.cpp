f2cbfa878fc92e8f276cc96f3011a86e336c8898
Bucher-Jeremy <jdbucher@live.com>
2014-07-02 17:06:00 -0700

    Signed-off-by: Bucher-Jeremy <jdbucher@live.com>

---
+++ jb2523887/Lab Work/savitch ch 2 prb4 example/main.cpp
@@ -0,0 +1,46 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ * illustrateqt
+ * Created on July 1, 2014, 10:11 AM
+ */
+
+//system libraries
+#include <iostream
+#include <iomanip>
+using namespace std;
+
+//user libraries
+
+//global constants
+const float CNVMYRS=1.0f/12.0f// conversion from months to years
+
+//Function Prototypes
+
+//Execution of code
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    //Declare variables
+    //Inputs
+    float oldsal,pincr;
+    unsigned short retmns;//number of months paid retroactively
+    //outputs
+    float newsal,retpay,mnpay;
+    
+    //input values
+    pincr=7.6e-2;
+    cout<<"Old Salary in dollars?"<<endl;
+    cin>>oldsal;
+    cot<<"Number of months for Retroactive Pay";
+    
+    //calculations
+    
+    //output the results
+    setprecision(2)
+    
+    //exit stage right
+    return 0;
+}
+
