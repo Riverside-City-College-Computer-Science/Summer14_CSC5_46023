0f6ebdafc032f84bea18c22a5ba9a1f0b54c8e9c
Kevirz <kevirz@gmail.com>
2014-07-11 18:10:08 -0700

    Signed-off-by: Kevirz <kevirz@gmail.com>

--- kr2437797/Assignment 4/Gaddis_7ThEd_Chap5_Problem5/main.cpp
+++ kr2437797/Assignment 4/Gaddis_7ThEd_Chap5_Problem5/main.cpp
@@ -6,27 +6,30 @@
  */
 
 #include <iostream>
+#include <iomanip>
+
 
 using namespace std;
 
 int main(int argc, char** argv) {
     
-    short yr;
-    float ppy;
-    float x=2500
+    //Declare Main Variables
+    float per=.04;              //Percentage Increase
     
-    
-
-    for(yr=0;yr<=6;yr++){
-        
-       ppy+=.04*x;
-       cout<<ppy;
+  
+    //For Statement which sets Year at O and Membership Costs (x) at $2500
+    //Also limits the loop to only show up to 6 Years of projected costs.
+    for(float yr=1, x=2500;yr<=6;yr++){
        
-       if(yr>=6)
-           cout<<endl;
-               
+       //Calculations
+       x+=per*x;                 //Outputs Membership costs
+      
+       //Ouputs Simple Text of the Year(yr)and the projected cost(x)
+       cout<<"Projected rates of Membership";
+       cout<<"for Year: "<<fixed<<setprecision(0)<<yr<<endl;
+       cout<<"Cost: $"<<fixed<<setprecision(2)<<x<<endl;
              
-    }
+}
     return 0;
 }
 
